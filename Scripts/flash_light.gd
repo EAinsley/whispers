68a2beb449b304814900bd31ffb2be850c3a4a43
Ainsley Su <vhtmscyo@gmail.com>
2024-10-20 10:09:52 +0200

    light flicker

---
+++ Scripts/flash_light.gd
@@ -0,0 +1,11 @@
+extends PointLight2D
+
+var time_elapes:= 0.
+var speed = 5.
+
+@onready var initial_energy = energy
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	time_elapes += delta
+	energy = initial_energy + sin(time_elapes * speed) * 0.3
